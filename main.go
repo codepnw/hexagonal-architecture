e32068ce8ea24f043b70b91bd0399d540bcc8fcd
codepnw <idchokms01@outlook.com>
2024-09-19 23:10:26 +0700

    config: added viper config .yaml

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
-	"os"
+	"strings"
+	"time"
 
 	"github.com/codepnw/hexagonal/handler"
 	"github.com/codepnw/hexagonal/repository"
@@ -10,21 +12,18 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
 	"github.com/jmoiron/sqlx"
-	"github.com/joho/godotenv"
+	"github.com/spf13/viper"
 )
 
 func main() {
-	godotenv.Load()
-
-	db, err := sqlx.Open("mysql", os.Getenv("DSN"))
-	if err != nil {
-		panic(err)
-	}
+	initTimezone()
+	initConfig()
+	db := initDatabase()
 
 	custRepoDB := repository.NewCustomerRepositoryDB(db)
-	_ = custRepoDB
 	custRepoMock := repository.NewCustomerRepositoryMock()
-	custService := service.NewCustomerService(custRepoMock)
+	_ = custRepoMock
+	custService := service.NewCustomerService(custRepoDB)
 	custHandler := handler.NewCustomerHandler(custService)
 
 	router := mux.NewRouter()
@@ -32,5 +31,51 @@ func main() {
 	router.HandleFunc("/customers", custHandler.GetCustomers).Methods(http.MethodGet)
 	router.HandleFunc("/customers/{id:[0-9]+}", custHandler.GetCustomer).Methods(http.MethodGet)
 
-	http.ListenAndServe(":8000", router)
+	port := fmt.Sprintf(":%d", viper.GetInt("app.port"))
+	fmt.Println("server starting at", port)
+	http.ListenAndServe(port, router)
+}
+
+func initConfig() {
+	viper.SetConfigName("example_config")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath(".")
+
+	viper.AutomaticEnv()
+	// example: APP_PORT=5000 go run .
+	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
+
+	if err := viper.ReadInConfig(); err != nil {
+		panic(err)
+	}
+}
+
+func initTimezone() {
+	ict, err := time.LoadLocation("Asia/Bangkok")
+	if err != nil {
+		panic(err)
+	}
+	time.Local = ict
+}
+
+func initDatabase() *sqlx.DB {
+	dsn := fmt.Sprintf(
+		"%s:%s@tcp(%s:%d)/%s?parseTime=true",
+		viper.GetString("db.username"),
+		viper.GetString("db.password"),
+		viper.GetString("db.host"),
+		viper.GetInt("db.port"),
+		viper.GetString("db.database"),
+	)
+
+	db, err := sqlx.Open(viper.GetString("db.driver"), dsn)
+	if err != nil {
+		panic(err)
+	}
+
+	db.SetConnMaxLifetime(2 * time.Minute)
+	db.SetMaxOpenConns(10)
+	db.SetMaxIdleConns(10)
+
+	return db
 }
