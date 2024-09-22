49a9e7c571567e00f6ff813f7fd6e3b403276c01
codepnw <idchokms01@outlook.com>
2024-09-23 00:33:46 +0700

    feat: account service

---
+++ service/account_service.go
@@ -0,0 +1,64 @@
+package service
+
+import (
+	"time"
+
+	"github.com/codepnw/hexagonal/errs"
+	"github.com/codepnw/hexagonal/logs"
+	"github.com/codepnw/hexagonal/repository"
+)
+
+type accountService struct {
+	accRepo repository.AccountRepository
+}
+
+func NewAccountService(accRepo repository.AccountRepository) AccountService {
+	return accountService{accRepo: accRepo}
+}
+
+func (s accountService) NewAccount(customerID int, request NewAccountRequest) (*AccountResponse, error) {
+	account := repository.Account{
+		CustomerID:  customerID,
+		OpeningDate: time.Now().Format("2006-01-02 15:04:05"),
+		AccountType: request.AccountType,
+		Amount:      request.Amount,
+		Status:      1,
+	}
+
+	newAcc, err := s.accRepo.Create(account)
+	if err != nil {
+		logs.Error(err)
+		return nil, errs.NewErrUnexpected()
+	}
+
+	response := AccountResponse{
+		AccountID:   newAcc.AccountID,
+		OpeningDate: newAcc.OpeningDate,
+		AccountType: newAcc.AccountType,
+		Amount:      newAcc.Amount,
+		Status:      newAcc.Status,
+	}
+
+	return &response, nil
+}
+
+func (s accountService) GetAccounts(customerID int) ([]AccountResponse, error) {
+	accounts, err := s.accRepo.GetAll(customerID)
+	if err != nil {
+		logs.Error(err)
+		return nil, errs.NewErrUnexpected()
+	}
+
+	responses := []AccountResponse{}
+
+	for _, acc := range accounts {
+		responses = append(responses, AccountResponse{
+			AccountID:   acc.AccountID,
+			OpeningDate: acc.OpeningDate,
+			AccountType: acc.AccountType,
+			Amount:      acc.Amount,
+			Status:      acc.Status,
+		})
+	}
+	return responses, nil
+}
